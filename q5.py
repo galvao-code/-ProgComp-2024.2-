ce41d8a2123fc0295070f8e91ad828e62101b8df
lovszksj <jullyana.azevedo@escolar.ifrn.edu.br>
2024-12-28 18:54:04 -0300

    Update q5.py

--- q5.py
+++ q5.py
@@ -1,18 +1,15 @@
 import datetime
 
 contador = 0
-data_zero = datetime.datetime(1968, 4, 27)
 hoje = datetime.datetime.today()
+data_zero = datetime.datetime(1968, 4, 27)
+
 
 total_dias = hoje - data_zero
 
-data_atual = data_zero
-#while data_atual <= hoje:
-    #if data_atual.weekday() == 5:  # 5 representa sábado
-        #contador += 1
-    #data_atual+= 1  # Incrementa um dia
+sabados = total_dias//7
 
 
 
-
-print (f'A quantidade de dias de {data_zero.date()} até {hoje.date()} é: {total_dias.days}')
+print(f'A quantidade de dias de {data_zero} até {hoje} é:  {total_dias}')
+print(f'e a quantidade de sábados é: {sabados}')
