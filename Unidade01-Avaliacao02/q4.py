c7d1d41226475581647bf0153df1b0bde8ac2e5f
lovszksj <jullyana.azevedo@escolar.ifrn.edu.br>
2025-01-02 17:18:40 -0300

    Update q4.py

---
+++ Unidade01-Avaliacao02/q4.py
@@ -0,0 +1,43 @@
+import random
+palavras = (
+    "ADAGA", "ADUBO", "AMIGO", "ANEXO", "ARAME", "ARARA", "ARROZ",
+    "ASILO", "ASTRO", "BAILE", "BAIXA", "BALAO", "BALSA", "BARCO",
+    "BARRO", "BEIJO", "BICHO", "BORDA", "BORRA", "BRAVO", "BREJO",
+    "BURRO", "CAIXA", "CALDO", "CANJA", "CARRO", "CARTA", "CERVO",
+    "CESTA", "CLIMA", "COBRA", "COLAR", "COQUE", "COURO", "CRAVO",
+    "DARDO", "FAIXA", "FARDO", "FENDA", "FERRO", "FESTA", "FLUOR",
+    "FORCA", "FORNO", "FORTE", "FUNDO", "GAITA", "GARRA", "GENIO",
+    "GESSO", "GRADE", "GRANA", "GRAMA", "GURIA", "GREVE", "GRUTA",
+    "HEROI", "HOTEL", "ICONE", "IMPAR", "IMUNE", "INDIO", "JUNTA",
+    "LAPIS", "LARVA", "LAZER", "LENTO", "LESTE", "LIMPO", "LIVRO",
+    "MACIO", "MAGRO", "MALHA", "MANSO", "MARCO", "METAL", "MORTE",
+    "MORRO", "MURAL", "MOVEL", "NACAO", "NINHO", "NOBRE", "NORMA",
+    "NORTE", "NUVEM", "PACTO", "PALHA", "PARDO", "PARTE", "PEDRA",
+    "PEDAL", "PEIXE", "PRADO", "PISTA", "POMBO", "POETA", "PONTO",
+    "PRATO", "PRECO", "PRESO", "PROSA", "PRUMO", "PULGA", "PULSO",
+    "QUEPE", "RAIVA", "RISCO", "RITMO", "ROSTO", "ROUPA", "SABAO",
+    "SALTO", "SENSO", "SINAL", "SITIO", "SONHO", "SOPRO", "SURDO",
+    "TARDE", "TERNO", "TERMO", "TERRA", "TIGRE", "TINTA", "TOLDO",
+    "TORRE", "TRAJE", "TREVO", "TROCO", "TRONO", "TURMA", "URUBU",
+    "VALSA", "VENTO", "VERDE", "VISAO", "VINHO", "VIUVO", "ZEBRA"
+)
+
+palavra1 = random.choice(palavras)
+palavra2 = random.choice(palavras)
+chutes = 5
+tentativa_p1 = 0
+tentativa_p2 = 0
+tentativa = 0
+
+while chutes >= 0:
+        print("\033[1;3m Digite todas as suas tentativas em caixa alta! Exemplo: PORTA")
+        tentativa = input("\033[m Revele o mistério, qual é a palavra? ")
+        if tentativa == palavra1 or tentativa == palavra2:
+            print("Impossível! Você é um gênio.")
+        else:
+            if chutes > 0:  
+                print(f"Poxa, não foi dessa vez! Você tem mais {chutes} tentativas")
+            else:
+                print("Foi por pouco, da próxima vez você consegue!")
+        chutes -= 1
+
